0cd9e618941c5a8ca11b7e637da252bd9ba63d80
Jherson De luquez <deluquezJhersonrgmail.com>
2024-05-05 14:42:39 -0500

    Creacion del controlador de Author, Creacion de view de Author y creacion de base de datos con info

---
+++ Library.Web/Controllers/AuthorsController.cs
@@ -0,0 +1,31 @@
+﻿using Library.Web.Data;
+using Library.Web.Data.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Library.Web.Controllers
+{
+    //los controladores en la programación sirven para coordinar la interacción entre el usuario,
+    //la interfaz de usuario y la lógica subyacente de una aplicación de software
+    public class AuthorsController : Controller
+    {
+        private readonly DataContext _context;
+
+        public AuthorsController(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            //Lista de autores en un metodo asincrono
+            IEnumerable<Author> list = await _context.Authors.ToListAsync();
+
+            return View(list);
+            //vamos a crear una vista 
+        }
+
+
+    }
+}
